5a5786e1982ee21b460dec20858dfc383f03ea3e
Anthony - Développement <92586179+AnthonyDeveloppement@users.noreply.github.com>
2021-10-15 15:56:26 +0200

    Add files via upload

---
+++ Lisez moi !.bat
@@ -0,0 +1,21 @@
+@echo off
+color c
+title jsfour-idcard By Anthony Developpement
+color b
+echo -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+echo                By : Anthony_#4197
+echo.
+echo -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+echo.
+echo.
+echo for help : https://discord.gg/E3m8AdJpqs
+echo.
+echo -------------------------------------------------
+echo.
+color d
+echo Creator : https://github.com/AnthonyDeveloppement
+echo.
+echo.
+echo -------------------------------------------------
+echo.
+pause
